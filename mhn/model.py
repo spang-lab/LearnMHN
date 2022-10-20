9ee5a32b2713e8b7ea2506520df9496d54de90d1
huy29433 <yanren-linda.hu@stud.uni-regensburg.de>
2022-10-20 17:01:07 +0200

    Added func for getting next bit

--- mhn/model.py
+++ mhn/model.py
@@ -2,6 +2,25 @@
 
 import numpy as np
 import pandas as pd
+class bits_fixed_n:
+
+    def __init__(self, n, k):
+        self.v = int("1"*n, 2)
+        self.stop_no = int("1"*n + "0"*(k-n), 2)
+        self.stop=False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.stop:
+            raise StopIteration
+        if self.v == self.stop_no:
+            self.stop=True
+        t = (self.v | (self.v - 1)) + 1
+        w = t | ((((t & -t)) // (self.v & (-self.v)) >> 1) - 1)
+        self.v, w = w, self.v
+        return w
 
 
 class MHN:
