3475fdf09cc88e5a04d8014cec509a3429d3321f
huy29433 <yanren-linda.hu@stud.uni-regensburg.de>
2022-11-08 16:16:41 +0100

    implemented mcmc

--- mhn/model.py
+++ mhn/model.py
@@ -410,6 +410,15 @@ class OmegaMHN(MHN):
             daxpy(n=nx, a=1, x=subdiag, incx=1, y=diag, incy=1)
         return diag
 
+    def order_prob(self, sigma):
+        events = np.zeros(self.log_theta.shape[0], dtype=np.int32)
+        events[sigma] = 1
+        sigma = np.array(sigma)
+        pos = np.argsort(np.argsort(sigma))
+        restr_diag = self.get_restr_diag(events=events)
+        return np.exp(sum((self.log_theta[x_i, sigma[:n_i]].sum() + self.log_theta[x_i, x_i]) for n_i, x_i in enumerate(sigma))) \
+            / np.prod([1 - restr_diag[(1 << pos)[:i].sum()] for i in range(len(sigma) + 1)])
+
     def likeliest_order(self, events: np.array):
 
         restr_diag = self.get_restr_diag(events=events)
@@ -437,41 +446,61 @@ class OmegaMHN(MHN):
 
         return (A, B)
 
-### THIS IS YIELDING WEIRD RESULTS, CHECK AGAIN
+    def mcmc_sampling(self, events: np.array, n_samples: int = 50, burn_in: float = 0.2):
 
-    def mcmc_sampling(self, events: np.array, n_samples: int=50, burn_in: float=0.2):
-                
-        
         restr_diag = self.get_restr_diag(events=events)
-        mutation_num = events.sum()  
+        mutation_num = events.sum()
 
         def proposal():
             S = np.nonzero(events)[0].tolist()
-            evs = []
-            p = 1
+            S_pos = list(range(len(S)))
+            sigma = []
+            Q_val = 1
             bin_state = 0
-            for __ in range(mutation_num): 
-                probs = np.exp(self.log_theta[np.ix_(S, evs)].sum(axis=1) + self.log_theta[S, S]) / (1 - restr_diag[bin_state + (1 << np.array(S))])
+            for _ in range(mutation_num):
+                probs = np.exp(self.log_theta[np.ix_(S, S)].sum(
+                    axis=0) - self.log_theta[S, S]) / (1 - restr_diag[bin_state + (1 << np.array(S_pos))])
                 i = np.random.choice(
                     np.arange(len(S)),
                     p=probs/probs.sum())
-                evs.append(S.pop(i))
-                p *= (probs[i] / probs.sum())
-                bin_state += (1 << evs[-1])
-            return evs, p
-        
-        samples = list()    
-        
+                sigma.append(S.pop(i))
+
+                Q_val *= (probs[i] / probs.sum())
+                bin_state += (1 << S_pos.pop(i))
+
+            return sigma, Q_val / self.order_prob(sigma=sigma)
+
+        samples = list()
+
         n = 0
-        evs, last_p = proposal()
-        samples.append(evs)
+        last_sigma, last_p = proposal()
+        samples.append(last_sigma)
 
         while n < int((1 + burn_in) * n_samples):
-            evs, p = proposal()
-            
+            sigma, p = proposal()
+
             if np.random.random() <= min(1, p/last_p):
-                samples.append(evs)
+                samples.append(sigma)
+                last_sigma = sigma
                 last_p = p
-                n += 1
-        
+            else:
+                samples.append(last_sigma)
+            n += 1
+
         return samples[-n_samples:]
+
+
+if __name__ == "__main__":
+    mhn = MHN.load(
+        R"C:\Users\Hu\Documents\Likeliestorder\likeliestorder\data\lung_no_split\log_theta")
+    events = np.zeros(mhn.log_theta.shape[0], dtype=np.int32)
+    events[:5] = 1
+    n_samples = 100
+    unique, counts = np.unique(mhn.mcmc_sampling(
+        events=events, n_samples=n_samples), return_counts=True, axis=0)
+    probs = {
+        tuple(u): c / n_samples for u, c in zip(unique, counts)
+    }
+    for k in sorted(probs, key=probs.get):
+        print(*k, end="  ")
+        print(probs[k])
