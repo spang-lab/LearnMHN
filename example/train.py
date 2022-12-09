c6a1692c78707888641d0e7609a074622c9501ae
huy29433 <hu.linda@aol.com>
2022-12-09 11:09:28 +0000

    rename and fix

--- example/train.py
+++ example/train.py
@@ -2,19 +2,21 @@ from mhn.optimizers import StateSpaceOptimizer
 import mhn.ssr.state_space_restriction as ssr
 import pandas as pd
 import numpy as np
+import os
 
 dataset = "G12_Breast_Prim"
+file_dir = os.path.dirname(os.path.abspath(__file__))
 
 if ssr.cuda_available() in [ssr.CUDA_NOT_AVAILABLE, ssr.CUDA_NOT_FUNCTIONAL]:
     raise RuntimeError(f"{ssr.cuda_available()}")
 
 
 opt = StateSpaceOptimizer()
-events = pd.read_csv(f"{dataset}/{dataset}_finalEvents.csv", index_col=0)
+events = pd.read_csv(f"{file_dir}/{dataset}/{dataset}_finalEvents.csv", index_col=0)
 
 opt.load_data_matrix(np.array(events, dtype=np.int32))
 
-opt.save_progress(steps=1, filename=f"{dataset}/progress.npy")
+opt.set_callback_func(lambda theta: print("Training step complete."))
 
 options = {
     # "lam": 0.0001,
