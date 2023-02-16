7f7e325df3af9eae4ece63217d028647829da859
Stefan V <stefan.email.2000@gmail.com>
2023-02-16 22:53:39 +0100

    copy of model.py

---
+++ mhn/model.py
@@ -0,0 +1,47 @@
+"""
+This submodule contains a class to represent Mutual Hazard Networks
+"""
+# author(s): Y. Linda Hu, Stefan Vocht
+
+import numpy as np
+import pandas as pd
+import json
+
+from typing import List
+
+
+class MHN:
+    """
+    This class represents the Mutual Hazard Network
+    """
+
+    def __init__(self, log_theta: np.array, events: List[str] = None, meta: dict = None):
+
+        self.log_theta = log_theta
+        self.events = events
+        self.meta = meta
+
+    def save(self, filename: str):
+        """
+        Save the MHN file
+        """
+        pd.DataFrame(self.log_theta, columns=self.events,
+                     index=self.events).to_csv(f"{filename}.csv")
+        if self.meta is not None:
+            with open(f"{filename}_meta.json", "x") as file:
+                json.dump(self.meta, file, indent=4)
+
+    @classmethod
+    def load(cls, filename: str, events: List[str] = None):
+        """
+        :param filename: path to the CSV file
+        """
+        df = pd.read_csv(f"{filename}.csv", index_col=0)
+        if events is None and (df.columns != pd.Index([str(x) for x in range(len(df.columns))])).any():
+            events = df.columns.to_list()
+        try:
+            with open(f"{filename}_meta.json", "r") as file:
+                meta = json.load(file)
+        except FileNotFoundError:
+            meta = None
+        return cls(np.array(df), events=events, meta=meta)
